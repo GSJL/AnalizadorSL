7619a7d702fc9a63dc7a784f006d75b91d456e43
GSJL <Serranoj721@gmail.com>
2021-10-26 20:05:10 -0500

    Numeros de mas de un digito

--- Analisador.py
+++ Analisador.py
@@ -12,7 +12,7 @@ class Analizador:
         i = 0
         while i < (len(operacion)):
             if operacion[i] == '+' or operacion[i]== '-' or operacion[i]== '*' or operacion[i]== '/' \
-                    or operacion[i]== '(' or operacion[i]== ')'or operacion[i]== '=':
+                    or operacion[i]== '(' or operacion[i]== ')':
                 self.__operadores.append(operacion[i])
                 self.__ord.append(1)
             elif ord(operacion[i]) > 47 and ord(operacion[i])< 58:
@@ -23,14 +23,30 @@ class Analizador:
                 i-=1
                 self.__constantes.append(res)
                 self.__ord.append(2)
-            elif operacion[i] != " ":
+            elif operacion[i] != " " and operacion[i] != "=":
                 self.__variables.append(operacion[i])
                 self.__ord.append(3)
             elif operacion[i] == '=':
-                self.__list_v.append(operacion[i+1])
+                res = ""
+                i+=1
+                if operacion[i] == " ":
+                    i+=1
+                    res = self.dobCaracter(operacion,i)
+                else:
+                    res = self.dobCaracter(operacion,i)
+                self.__list_v.append(res)
                 self.__ord.append(4)
+                i+=1
             i+=1
 
+
+    def dobCaracter(operacion, i):
+        res = ""
+        while ord(operacion[i]) > 47 and ord(operacion[i])< 58:
+            res+= operacion[i]
+            i+=1
+        i-=1
+        return res
 
 
     def printTest(self):
